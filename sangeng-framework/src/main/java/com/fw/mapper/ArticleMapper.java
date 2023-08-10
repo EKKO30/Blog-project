b7394c62f3e6ac9e3daa439fcbda5e67e6e48ae4
EKKO30 <122966150+EKKO30@users.noreply.github.com>
2023-08-10 14:22:10 +0800

    Update ArticleMapper.java

--- sangeng-framework/src/main/java/com/fw/mapper/ArticleMapper.java
+++ sangeng-framework/src/main/java/com/fw/mapper/ArticleMapper.java
@@ -9,4 +9,7 @@ public interface ArticleMapper extends BaseMapper<Article> {
 
     @Update("UPDATE sg_article SET del_flag=1 WHERE id=#{id} AND del_flag=0")
     int UpdateDel_flag(Long id);
+
+        @Update("UPDATE sg_article\n SET view_count = #{viewCount} WHERE id = #{id})
+    void UpdateV(@Param("id") Long id,@Param("viewCount") Long viewCount);
 }
